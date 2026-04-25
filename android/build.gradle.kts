4fbdd465fdaee399445bbbcf3462fa89079f09e5
Monaum-202 <monaum.202@gmail.com>
2026-04-25 07:41:26 +0600

    build error issue

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,22 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.getByName("android")
+            try {
+                val getNamespace = android.javaClass.getMethod("getNamespace")
+                if (getNamespace.invoke(android) == null) {
+                    val setNamespace = android.javaClass.getMethod("setNamespace", String::class.java)
+                    setNamespace.invoke(android, "dev.isar.${project.name.replace("_", ".")}")
+                }
+            } catch (e: Exception) {
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
