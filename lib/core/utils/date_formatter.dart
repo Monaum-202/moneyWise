906481a52028dc0d5fa6c7e241f2e063667afc92
Monaum-202 <monaum.202@gmail.com>
2026-04-26 23:10:31 +0600

    date formatter

--- lib/core/utils/date_formatter.dart
+++ lib/core/utils/date_formatter.dart
@@ -1,11 +1,15 @@
 import 'package:intl/intl.dart';
 
 class DateFormatter {
-  static String format(DateTime date) {
-    return DateFormat.yMMMMd().format(date);
+  static String format(DateTime date, {String pattern = 'dd MMM yyyy'}) {
+    return DateFormat(pattern).format(date);
   }
 
-  static String formatShort(DateTime date) {
-    return DateFormat.yMd().format(date);
+  static String formatMonthYear(DateTime date) {
+    return DateFormat('MMMM yyyy').format(date);
+  }
+
+  static String formatDayMonth(DateTime date) {
+    return DateFormat('dd MMM').format(date);
   }
 }
